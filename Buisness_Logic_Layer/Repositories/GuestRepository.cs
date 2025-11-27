b1491019a2d013c5674958da8b3a6f04b809f921
Abdelrahman Atef <3obade2004@gmail.com>
2025-11-28 01:20:46 +0200

    Update the BLL

---
+++ Buisness_Logic_Layer/Repositories/GuestRepository.cs
@@ -0,0 +1,14 @@
+﻿using Buisness_Logic_Layer.Interfaces;
+using Data_Access_Layer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buisness_Logic_Layer.Repositories
+{
+    public class GuestRepository : GenericRepository<Guest>, IGuestRepository
+    {
+    }
+}
