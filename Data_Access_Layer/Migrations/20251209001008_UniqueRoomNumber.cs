28d9040b0b01a8f600ddd5e56e89f2972c4be7c8
INeedAJobASAP <Mohammedseleem2005@gmail.com>
2025-12-09 18:14:27 +0200

    Add migrations and new roomtype images

---
+++ Data_Access_Layer/Migrations/20251209001008_UniqueRoomNumber.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data_Access_Layer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueRoomNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Rooms_RoomId",
+                table: "Booking");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_RoomNumber",
+                table: "Rooms",
+                column: "RoomNumber",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Rooms_RoomId",
+                table: "Booking",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Rooms_RoomId",
+                table: "Booking");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rooms_RoomNumber",
+                table: "Rooms");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Rooms_RoomId",
+                table: "Booking",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
