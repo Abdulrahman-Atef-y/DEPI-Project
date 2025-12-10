7a4cf97333c43521acb6571ed21a1fd5b4a0adf4
INeedAJobASAP <Mohammedseleem2005@gmail.com>
2025-12-10 05:39:17 +0200

    added EmailService Repository

---
+++ Buisness_Logic_Layer/Repositories/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using Buisness_Logic_Layer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buisness_Logic_Layer.Repositories
+{
+    public class EmailService : IEmailService
+    {
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            using (var client = new SmtpClient("smtp.example.com", 587))
+            {
+                client.Credentials = new NetworkCredential("your_email@example.com", "your_password");
+                client.EnableSsl = true;
+
+                var mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress("your_email@example.com");
+                mailMessage.To.Add(toEmail);
+                mailMessage.Subject = subject;
+                mailMessage.Body = body;
+
+                await client.SendMailAsync(mailMessage);
+            }
+        }
+    }
+}
